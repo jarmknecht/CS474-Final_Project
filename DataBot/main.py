97954b44a616b48a79cd7f6e81a92cd1fe256b52
greensurfer <austinkolander@gmail.com>
2019-11-11 17:06:17 -0700

    Thread bug updates!

--- DataBot/main.py
+++ DataBot/main.py
@@ -37,24 +37,29 @@ def main():
         ticker_data = json.load(json_file)
 
     # Start
-    tickers = ["AAPL", "GOOG", "FB", "F", "GE", "SNAP"]
+    #tickers = ["AAPL", "GOOG", "FB", "F", "GE", "SNAP"]
     tickers = ["AAPL"]
-    # tickers = []
-    # for key, value in ticker_data.items():
-    #     tickers.append(value)
+    tickers = []
+    for key, value in ticker_data.items():
+        tickers.append(value)
 
     #if args.path is not None:
     #   DATA_DIR = args.path
 
     if args.quotes is not None:
-        stock_downloader = Thread(target=stock_price_workflow(args.quotes, tickers), daemon=True).start()
+        stock_downloader = Thread(target=stock_price_workflow, args=(args.quotes, tickers), daemon=True)
         stock_downloader.start()
 
     if args.news is not None:
-        news_downloader = Thread(target=stock_news_workflow(args.news, tickers), daemon=True).start()
+        news_downloader = Thread(target=stock_news_workflow, args=(args.news, tickers), daemon=True)
+        news_downloader.start()
     if args.social is not None:
         social_downloader = Thread(target=stock_social_workflow(args.social, tickers), daemon=True)
         social_downloader.start()
+
+    stock_downloader.join()
+    news_downloader.join()
+    social_downloader.join()
 
 
 def stock_price_workflow(quote_args, tickers):
